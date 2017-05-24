7022db03c805c3a356a5a16eb45ea86a3b8934bc
Matthias Steinbauer <matthias@steinbauer.org>
2017-05-24 14:13:27 +0200

    Basic navigation between start and angry screen

--- test-your-fear.js
+++ test-your-fear.js
@@ -1,8 +1,29 @@
 /* Test your fear implementation */
 
 var tyf = {
+    
+    /** Show a slide and hide all others */
+    showSlide(slideId) {
+        if(!slideId.startsWith('#')) {
+            slideId = '#' + slideId;
+        }
+        $('.slide').addClass('hidden');
+        $(slideId).removeClass('hidden');
+    },
+    
+    /** Setup the callback handlers for all buttons and slides */
+    setupHandlers() {
+        $('#ichhabeangst').click(function() {
+            tyf.showSlide('schimpfwort');
+            setTimeout(function() {
+                tyf.showSlide('startscreen');
+            }, 3000);
+        });
+    },
 	
 	init: function() {
+        this.setupHandlers();
+        this.showSlide('startscreen');
 	}
 
 };
