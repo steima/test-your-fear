18f5eadabc926c20a8c277ea93bde8a1f02b6d1d
Matthias Steinbauer <matthias@steinbauer.org>
2017-05-24 14:20:21 +0200

    Select curse word at random

--- test-your-fear.js
+++ test-your-fear.js
@@ -2,6 +2,36 @@
 
 var tyf = {
     
+    /** List of available curse words */
+    shimpfwoerter: [
+        'Du Weichei!',
+        'Du Warmd- uscher!',
+        'Du Angst- hase!',
+        'Du feige Sau!',
+        'Du Lulu!',
+        'Du Security Freak!',
+        'Du Drücke- berger!',
+        'Du Wasch- lappen!',
+        'Du Becken- rand- schwim- mer!',
+        'Du Schisser!',
+        'Du fade Nudl!',
+        'Du Kakao- trinker!',
+        'Du Karusell- bremser!',
+        'Du Spielver- derber!',
+        'Du Socken- sortierer!'
+    ],
+    
+    /** Select a string at random from an array */
+    selectRandomFromArray(arr) {
+        var r = Math.floor(Math.random()*arr.length);
+        return arr[r];
+    },
+    
+    /** Select a curse word at random */
+    selectSchimpfwort() {
+        return this.selectRandomFromArray(this.shimpfwoerter);
+    },
+    
     /** Show a slide and hide all others */
     showSlide(slideId) {
         if(!slideId.startsWith('#')) {
@@ -14,6 +44,7 @@ var tyf = {
     /** Setup the callback handlers for all buttons and slides */
     setupHandlers() {
         $('#ichhabeangst').click(function() {
+            $('#beschimpfung').html(tyf.selectSchimpfwort());
             tyf.showSlide('schimpfwort');
             setTimeout(function() {
                 tyf.showSlide('startscreen');
